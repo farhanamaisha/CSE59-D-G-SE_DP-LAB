2cfc28956caa48b252d888a510fc35cea09406eb
Farhana Maisha Chowdhury <farhanamaishachy@gmail.com>
2026-06-18 16:06:58 +0600

    Update Main5.java

--- Task 2/Main5.java
+++ Task 2/Main5.java
@@ -1,1 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Gcl
+ */
+class p{
+    void pd(String text){
+        System.out.println("text"+ text);
+    }
+    void pd(int n){
+        System.out.println("number"+ n);
+    }
+} 
+public class Main5 {
+    public static void main(String[] args) {
+        p pr=new p();
+        pr.pd("hello java");
+        pr.pd(100);
+    }
+}
 
