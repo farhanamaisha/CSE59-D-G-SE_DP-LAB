758b59fb93e94ded2ef67431d8657812df24f275
Farhana Maisha Chowdhury <farhanamaishachy@gmail.com>
2026-06-18 15:55:18 +0600

    Update Main3.java

--- Task 2/Main3.java
+++ Task 2/Main3.java
@@ -1,1 +1,23 @@
 
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Gcl
+ */
+class countdown{
+    void start(){
+        for(int i=5;i>=1;i--){
+        System.out.println(i);}
+        System.out.println("go");
+    }
+}
+public class Main3 {
+    public static void main(String[] args) {
+        countdown countdown =new countdown();
+        countdown.start();
+    }
+}
