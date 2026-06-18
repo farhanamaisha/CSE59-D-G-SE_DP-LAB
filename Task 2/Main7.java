a9d0c5e354c23b9008495aa448b6ec043dda0747
Farhana Maisha Chowdhury <farhanamaishachy@gmail.com>
2026-06-18 16:15:15 +0600

    Update Main7.java

--- Task 2/Main7.java
+++ Task 2/Main7.java
@@ -1,1 +1,24 @@
 
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author Gcl
+ */
+interface clickable{
+    void onclick();
+}
+class button implements clickable{
+    public void onclick(){
+        System.out.println("button was clicked");
+    }
+}
+public class Main7 {
+    public static void main(String[] args) {
+        button button =new button();
+        button .onclick();
+    }
+}
