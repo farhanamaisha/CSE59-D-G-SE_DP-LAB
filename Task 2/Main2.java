6e683426de3e8c8c37ffb6dd7b0c118ee9556713
Farhana Maisha Chowdhury <farhanamaishachy@gmail.com>
2026-06-18 15:44:54 +0600

    Update Main2.java

--- Task 2/Main2.java
+++ Task 2/Main2.java
@@ -1,1 +1,21 @@
 
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Gcl
+ */
+class numberchecker{
+    void check (int num){
+        boolean result=(num%2==0)&&(num>10);
+        System.out.println("result");
+    }
+}
+public class Main2 {
+    public static void main(String[] args) {
+        numberchecker checker=new numberchecker();
+        checker.check(14);}
+}
