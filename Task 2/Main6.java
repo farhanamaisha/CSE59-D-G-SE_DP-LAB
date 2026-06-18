b26030be6aebd8d4426ae673265e4e46d00a0caa
Farhana Maisha Chowdhury <farhanamaishachy@gmail.com>
2026-06-18 16:10:43 +0600

    Update Main6.java

--- Task 2/Main6.java
+++ Task 2/Main6.java
@@ -1,1 +1,29 @@
 
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Gcl
+ */
+class applet{
+    void launch(){
+        System.out.println("appled launch");
+    }
+    
+}
+class gameapplet extends applet{
+    void play(){
+        System.out.println("playing game");
+    }
+}
+
+public class Main6 {
+    public static void main(String[] args) {
+        gameapplet game=new gameapplet();
+        game.launch();
+        game. play();
+    }
+}
