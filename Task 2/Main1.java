10747a7a6a6aa050a4e1dde95ff91c7d92e1abf0
Farhana Maisha Chowdhury <farhanamaishachy@gmail.com>
2026-06-18 15:50:06 +0600

    Update Main1.java

--- Task 2/Main1.java
+++ Task 2/Main1.java
@@ -1,1 +1,23 @@
 
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Gcl
+ */
+class conteststats{
+    int score[]={85,90,95};
+
+void p(){
+System.out.println("first score"+ score[0]);
+}}
+public class Main1 {
+    public static void main(String[] args) {
+        conteststats contest=new conteststats();
+        contest.p();
+    }
+ 
+}
