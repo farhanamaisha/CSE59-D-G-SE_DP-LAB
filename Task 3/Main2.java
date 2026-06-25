fc380ac2efc9ad95a5156c38926e318bf0109e89
Farhana Maisha Chowdhury <farhanamaishachy@gmail.com>
2026-06-25 16:33:43 +0600

    Update Main2.java

--- Task 3/Main2.java
+++ Task 3/Main2.java
@@ -1,42 +1,54 @@
-
-public class User {
-    private String username;
-    private String email;
-    public User(String username,String email){
-        this.username=username;
-        this.email=email;
-    }
-    public String getusername(){
-        return username;
-    }
-    public String getemail(){
-        return email;
-    }
-}
-public class EmailValidator {
-    public static boolean isvalid(String email){
-        if(email!=null && email.contains("@")){
-            return true;
-        
-        }
-        System.out.println("invalid email format");
-        return false;
-    }
-    
-}
-public class UserRepository {
-    public void save(User user){
-        if(EmailValidator.isvalid(user.getemail())){
-            System.out.println("connecting database");
-            System.out.println("saving user"+ user.getusername()+ "to the user table");
-        }
-    }
-}
-
 public class Main2 {
+
+    
+    static class User {
+        private String username;
+        private String email;
+
+        public User(String username, String email) {
+            this.username = username;
+            this.email = email;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+    }
+
+    
+    static class EmailValidator {
+        public static boolean isValid(String email) {
+            if (email != null && email.contains("@")) {
+                return true;
+            }
+
+            System.out.println("Invalid email format.");
+            return false;
+        }
+    }
+
+    
+    static class UserRepository {
+        public void save(User user) {
+            if (EmailValidator.isValid(user.getEmail())) {
+                System.out.println("Connecting to database...");
+                System.out.println(
+                    "Saving user " + user.getUsername() + " to the users table."
+                );
+            }
+        }
+    }
+
+    
     public static void main(String[] args) {
-        User user =new User ("john doe","a@exm.com");
-        UserRepository rep=new  UserRepository();
-        rep.save(user);
+
+        User user = new User("John", "john@example.com");
+
+        UserRepository repository = new UserRepository();
+        repository.save(user);
     }
 }
