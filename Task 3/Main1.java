5a9208895bcf4f386299e12dec94a8f6045aa667
Farhana Maisha Chowdhury <farhanamaishachy@gmail.com>
2026-06-25 16:32:00 +0600

    Update Main1.java

--- Task 3/Main1.java
+++ Task 3/Main1.java
@@ -1,57 +1,82 @@
-public interface Shape {
-    double CalculateArea();
-}
-public class Rectangle implements Shape {
-    private double length;
-    private double width;
-    
-    public Rectangle(double length,double width){
-        this.length=length;
-        this.width=width;
-    }
-    public double CalculateArea(){
-        return length*width;
-    }
-}
-public class Circle implements Shape{
-    private double r;
-    public Circle (double r){
-        this.r=r;
-    }
-    public double CalculateArea(){
-        return Math.PI*r*r;
-    }
-}
-public class Triangle implements Shape {
-    private double b;
-    private double h;
-    public Triangle(double b,double h){
-        this.b=b;
-                this.h=h;}
-    public double CalculateArea(){
-        return  0.5*b*h;
-    }
-}
-public class AreaCalculator {
-    public double CalculateArea(Shape[]shapes){
-        double totalarea=0;
-        for (Shape shape:shapes){
-            totalarea+=shape.CalculateArea();
-            
-        }
-        return totalarea;
-    }
-            
-}
-
 public class Main1 {
+
+    
+    interface Shape {
+        double calculateArea();
+    }
+
+    
+    static class Rectangle implements Shape {
+        private double length;
+        private double width;
+
+        public Rectangle(double length, double width) {
+            this.length = length;
+            this.width = width;
+        }
+
+        @Override
+        public double calculateArea() {
+            return length * width;
+        }
+    }
+
+  
+    static class Circle implements Shape {
+        private double radius;
+
+        public Circle(double radius) {
+            this.radius = radius;
+        }
+
+        @Override
+        public double calculateArea() {
+            return Math.PI * radius * radius;
+        }
+    }
+
+   
+    static class Triangle implements Shape {
+        private double base;
+        private double height;
+
+        public Triangle(double base, double height) {
+            this.base = base;
+            this.height = height;
+        }
+
+        @Override
+        public double calculateArea() {
+            return 0.5 * base * height;
+        }
+    }
+
+    
+    static class AreaCalculator {
+        public double calculateTotalArea(Shape[] shapes) {
+            double totalArea = 0;
+
+            for (Shape shape : shapes) {
+                totalArea += shape.calculateArea();
+            }
+
+            return totalArea;
+        }
+    }
+
+  
     public static void main(String[] args) {
-        Shape[]shapes={
-            new Rectangle(5,4),
-            new Circle (3),
-            new Triangle(6,2)
+
+        Shape[] shapes = {
+            new Rectangle(5, 4),
+            new Circle(3),
+            new Triangle(6, 2)
         };
-        AreaCalculator  calculator=new AreaCalculator();
-        System.out.println("total area"+ calculator.CalculateArea(shapes));
+
+        AreaCalculator calculator = new AreaCalculator();
+
+        double totalArea = calculator.calculateTotalArea(shapes);
+
+        System.out.println("Total Area = " + totalArea);
     }
 }
