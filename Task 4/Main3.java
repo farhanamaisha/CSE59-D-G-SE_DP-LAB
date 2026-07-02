f334cf7ba8d5771da533448997b506e0e95d545d
Farhana Maisha Chowdhury <farhanamaishachy@gmail.com>
2026-07-02 16:00:30 +0600

    Update Main3.java

--- Task 4/Main3.java
+++ Task 4/Main3.java
@@ -1,1 +1,34 @@
 
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Gcl
+ */
+public class Main3 {
+    interface sender{
+        void send (String m);
+    }
+    static class emailsender implements sender{
+        public void send(String m){
+            System.out.println(" email"+ m);
+        }
+    }
+    static class notification{
+        sender s;
+        notification (sender s){
+            this.s=s;
+        }
+        void alert(String m){
+            s.send(m);
+        }
+    }
+    public static void main(String[] args) {
+        sender e=new emailsender();
+        notification n=new notification(e);
+        n.alert("welcome");
+    }
+}
